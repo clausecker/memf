9842b8455b8706696a6f88ceea873b6ac0744aba
Robert Clausecker <fuzxxl@gmail.com>
2015-03-07 15:09:00 +0100

    Remove rwnop function
    If dir is NOP, rwfunc isn't ever used.

--- nopf.c
+++ nopf.c
@@ -3,22 +3,6 @@
 #include "memf.h"
 #include "memf-internal.h"
 
-static rwfunc rwnop;
-
-/*
- * Just pretend to do something.
- */
-static size_t
-rwnop(void *file, uint8_t *buf, size_t count)
-{
-
-	(void)buf;
-	(void)file;
-	(void)count;
-
-	return count;
-}
-
 /*
  * Do a dry-run: do not read or write any data but compute how much
  * data is read / written. This function can set errno to EINVAL when
@@ -27,7 +11,7 @@ rwnop(void *file, uint8_t *buf, size_t count)
 extern size_t
 nopf(const char *fstr)
 {
-	struct rwfile rwf = { &rwnop, NULL, NOP };
+	struct rwfile rwf = {NULL, NULL, NOP};
 
 	return memf(&rwf, fstr, NULL);
 }
